b8d2ce2994550a2544fed1deb40dcf1cc48992b4
shnyder <ajs.private@gmail.com>
2021-10-24 22:05:33 +0200

    lintered

--- headless-tables-and-selection-filters/src/generated/graphql.tsx
+++ headless-tables-and-selection-filters/src/generated/graphql.tsx
@@ -1,10 +1,16 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions =  {}
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+const defaultOptions = {};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -23,28 +29,27 @@ export type Character = {
   /** The friends of the character exposed as a connection with edges */
   friendsConnection: FriendsConnection;
   /** The ID of the character */
-  id: Scalars['ID'];
+  id: Scalars["ID"];
   /** The name of the character */
-  name: Scalars['String'];
+  name: Scalars["String"];
 };
-
 
 /** A character from the Star Wars universe */
 export type CharacterFriendsConnectionArgs = {
-  after?: Maybe<Scalars['ID']>;
-  first?: Maybe<Scalars['Int']>;
+  after?: Maybe<Scalars["ID"]>;
+  first?: Maybe<Scalars["Int"]>;
 };
 
 /** The input object sent when passing in a color */
 export type ColorInput = {
-  blue: Scalars['Int'];
-  green: Scalars['Int'];
-  red: Scalars['Int'];
+  blue: Scalars["Int"];
+  green: Scalars["Int"];
+  red: Scalars["Int"];
 };
 
 /** An autonomous mechanical character in the Star Wars universe */
 export type Droid = Character & {
-  __typename?: 'Droid';
+  __typename?: "Droid";
   /** The movies this droid appears in */
   appearsIn: Array<Maybe<Episode>>;
   /** This droid's friends, or an empty list if they have none */
@@ -52,33 +57,32 @@ export type Droid = Character & {
   /** The friends of the droid exposed as a connection with edges */
   friendsConnection: FriendsConnection;
   /** The ID of the droid */
-  id: Scalars['ID'];
+  id: Scalars["ID"];
   /** What others call this droid */
-  name: Scalars['String'];
+  name: Scalars["String"];
   /** This droid's primary function */
-  primaryFunction?: Maybe<Scalars['String']>;
+  primaryFunction?: Maybe<Scalars["String"]>;
 };
-
 
 /** An autonomous mechanical character in the Star Wars universe */
 export type DroidFriendsConnectionArgs = {
-  after?: Maybe<Scalars['ID']>;
-  first?: Maybe<Scalars['Int']>;
+  after?: Maybe<Scalars["ID"]>;
+  first?: Maybe<Scalars["Int"]>;
 };
 
 /** The episodes in the Star Wars trilogy */
 export enum Episode {
   /** Star Wars Episode V: The Empire Strikes Back, released in 1980. */
-  Empire = 'EMPIRE',
+  Empire = "EMPIRE",
   /** Star Wars Episode VI: Return of the Jedi, released in 1983. */
-  Jedi = 'JEDI',
+  Jedi = "JEDI",
   /** Star Wars Episode IV: A New Hope, released in 1977. */
-  Newhope = 'NEWHOPE'
+  Newhope = "NEWHOPE",
 }
 
 /** A connection object for a character's friends */
 export type FriendsConnection = {
-  __typename?: 'FriendsConnection';
+  __typename?: "FriendsConnection";
   /** The edges for each of the character's friends. */
   edges?: Maybe<Array<Maybe<FriendsEdge>>>;
   /** A list of the friends, as a convenience when edges are not needed. */
@@ -86,21 +90,21 @@ export type FriendsConnection = {
   /** Information for paginating this connection */
   pageInfo: PageInfo;
   /** The total number of friends */
-  totalCount?: Maybe<Scalars['Int']>;
+  totalCount?: Maybe<Scalars["Int"]>;
 };
 
 /** An edge object for a character's friends */
 export type FriendsEdge = {
-  __typename?: 'FriendsEdge';
+  __typename?: "FriendsEdge";
   /** A cursor used for pagination */
-  cursor: Scalars['ID'];
+  cursor: Scalars["ID"];
   /** The character represented by this friendship edge */
   node?: Maybe<Character>;
 };
 
 /** A humanoid creature from the Star Wars universe */
 export type Human = Character & {
-  __typename?: 'Human';
+  __typename?: "Human";
   /** The movies this human appears in */
   appearsIn: Array<Maybe<Episode>>;
   /** This human's friends, or an empty list if they have none */
@@ -108,26 +112,24 @@ export type Human = Character & {
   /** The friends of the human exposed as a connection with edges */
   friendsConnection: FriendsConnection;
   /** Height in the preferred unit, default is meters */
-  height?: Maybe<Scalars['Float']>;
+  height?: Maybe<Scalars["Float"]>;
   /** The home planet of the human, or null if unknown */
-  homePlanet?: Maybe<Scalars['String']>;
+  homePlanet?: Maybe<Scalars["String"]>;
   /** The ID of the human */
-  id: Scalars['ID'];
+  id: Scalars["ID"];
   /** Mass in kilograms, or null if unknown */
-  mass?: Maybe<Scalars['Float']>;
+  mass?: Maybe<Scalars["Float"]>;
   /** What this human calls themselves */
-  name: Scalars['String'];
+  name: Scalars["String"];
   /** A list of starships this person has piloted, or an empty list if none */
   starships?: Maybe<Array<Maybe<Starship>>>;
 };
 
-
 /** A humanoid creature from the Star Wars universe */
 export type HumanFriendsConnectionArgs = {
-  after?: Maybe<Scalars['ID']>;
-  first?: Maybe<Scalars['Int']>;
+  after?: Maybe<Scalars["ID"]>;
+  first?: Maybe<Scalars["Int"]>;
 };
-
 
 /** A humanoid creature from the Star Wars universe */
 export type HumanHeightArgs = {
@@ -137,17 +139,16 @@ export type HumanHeightArgs = {
 /** Units of height */
 export enum LengthUnit {
   /** Primarily used in the United States */
-  Foot = 'FOOT',
+  Foot = "FOOT",
   /** The standard unit around the world */
-  Meter = 'METER'
+  Meter = "METER",
 }
 
 /** The mutation type, represents all updates we can make to our data */
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   createReview?: Maybe<Review>;
 };
-
 
 /** The mutation type, represents all updates we can make to our data */
 export type MutationCreateReviewArgs = {
@@ -157,15 +158,15 @@ export type MutationCreateReviewArgs = {
 
 /** Information for paginating this connection */
 export type PageInfo = {
-  __typename?: 'PageInfo';
-  endCursor?: Maybe<Scalars['ID']>;
-  hasNextPage: Scalars['Boolean'];
-  startCursor?: Maybe<Scalars['ID']>;
+  __typename?: "PageInfo";
+  endCursor?: Maybe<Scalars["ID"]>;
+  hasNextPage: Scalars["Boolean"];
+  startCursor?: Maybe<Scalars["ID"]>;
 };
 
 /** The query type, represents all of the entry points into our object graph */
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   character?: Maybe<Character>;
   droid?: Maybe<Droid>;
   hero?: Maybe<Character>;
@@ -175,82 +176,74 @@ export type Query = {
   starship?: Maybe<Starship>;
 };
 
-
 /** The query type, represents all of the entry points into our object graph */
 export type QueryCharacterArgs = {
-  id: Scalars['ID'];
+  id: Scalars["ID"];
 };
-
 
 /** The query type, represents all of the entry points into our object graph */
 export type QueryDroidArgs = {
-  id: Scalars['ID'];
+  id: Scalars["ID"];
 };
-
 
 /** The query type, represents all of the entry points into our object graph */
 export type QueryHeroArgs = {
   episode?: Maybe<Episode>;
 };
 
-
 /** The query type, represents all of the entry points into our object graph */
 export type QueryHumanArgs = {
-  id: Scalars['ID'];
+  id: Scalars["ID"];
 };
-
 
 /** The query type, represents all of the entry points into our object graph */
 export type QueryReviewsArgs = {
   episode: Episode;
 };
 
-
 /** The query type, represents all of the entry points into our object graph */
 export type QuerySearchArgs = {
-  text?: Maybe<Scalars['String']>;
+  text?: Maybe<Scalars["String"]>;
 };
-
 
 /** The query type, represents all of the entry points into our object graph */
 export type QueryStarshipArgs = {
-  id: Scalars['ID'];
+  id: Scalars["ID"];
 };
 
 /** Represents a review for a movie */
 export type Review = {
-  __typename?: 'Review';
+  __typename?: "Review";
   /** Comment about the movie */
-  commentary?: Maybe<Scalars['String']>;
+  commentary?: Maybe<Scalars["String"]>;
   /** The movie */
   episode?: Maybe<Episode>;
   /** The number of stars this review gave, 1-5 */
-  stars: Scalars['Int'];
+  stars: Scalars["Int"];
 };
 
 /** The input object sent when someone is creating a new review */
 export type ReviewInput = {
   /** Comment about the movie, optional */
-  commentary?: Maybe<Scalars['String']>;
+  commentary?: Maybe<Scalars["String"]>;
   /** Favorite color, optional */
   favorite_color?: Maybe<ColorInput>;
   /** 0-5 stars */
-  stars: Scalars['Int'];
+  stars: Scalars["Int"];
 };
 
 export type SearchResult = Droid | Human | Starship;
 
 export type Starship = {
-  __typename?: 'Starship';
-  coordinates?: Maybe<Array<Array<Scalars['Float']>>>;
+  __typename?: "Starship";
+  coordinates?: Maybe<Array<Array<Scalars["Float"]>>>;
   /** The ID of the starship */
-  id: Scalars['ID'];
+  id: Scalars["ID"];
   /** Length of the starship, along the longest axis */
-  length?: Maybe<Scalars['Float']>;
+  length?: Maybe<Scalars["Float"]>;
   /** The name of the starship */
-  name: Scalars['String'];
+  name: Scalars["String"];
 };
-
 
 export type StarshipLengthArgs = {
   unit?: Maybe<LengthUnit>;
@@ -258,49 +251,111 @@ export type StarshipLengthArgs = {
 
 /** The subscription type, represents all subscriptions we can make to our data */
 export type Subscription = {
-  __typename?: 'Subscription';
+  __typename?: "Subscription";
   reviewAdded?: Maybe<Review>;
 };
-
 
 /** The subscription type, represents all subscriptions we can make to our data */
 export type SubscriptionReviewAddedArgs = {
   episode?: Maybe<Episode>;
 };
 
-export type GetAllHerosQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetAllHerosQueryVariables = Exact<{ [key: string]: never }>;
 
-
-export type GetAllHerosQuery = { __typename?: 'Query', hero?: { __typename: 'Droid', id: string, name: string, friendsConnection: { __typename: 'FriendsConnection', totalCount?: number | null | undefined, edges?: Array<{ __typename: 'FriendsEdge', cursor: string, node?: { __typename: 'Droid', id: string, name: string } | { __typename: 'Human', id: string, name: string } | null | undefined } | null | undefined> | null | undefined, pageInfo: { __typename: 'PageInfo', startCursor?: string | null | undefined, endCursor?: string | null | undefined } } } | { __typename: 'Human', id: string, name: string, friendsConnection: { __typename: 'FriendsConnection', totalCount?: number | null | undefined, edges?: Array<{ __typename: 'FriendsEdge', cursor: string, node?: { __typename: 'Droid', id: string, name: string } | { __typename: 'Human', id: string, name: string } | null | undefined } | null | undefined> | null | undefined, pageInfo: { __typename: 'PageInfo', startCursor?: string | null | undefined, endCursor?: string | null | undefined } } } | null | undefined };
-
+export type GetAllHerosQuery = {
+  __typename?: "Query";
+  hero?:
+    | {
+        __typename: "Droid";
+        id: string;
+        name: string;
+        friendsConnection: {
+          __typename: "FriendsConnection";
+          totalCount?: number | null | undefined;
+          edges?:
+            | Array<
+                | {
+                    __typename: "FriendsEdge";
+                    cursor: string;
+                    node?:
+                      | { __typename: "Droid"; id: string; name: string }
+                      | { __typename: "Human"; id: string; name: string }
+                      | null
+                      | undefined;
+                  }
+                | null
+                | undefined
+              >
+            | null
+            | undefined;
+          pageInfo: {
+            __typename: "PageInfo";
+            startCursor?: string | null | undefined;
+            endCursor?: string | null | undefined;
+          };
+        };
+      }
+    | {
+        __typename: "Human";
+        id: string;
+        name: string;
+        friendsConnection: {
+          __typename: "FriendsConnection";
+          totalCount?: number | null | undefined;
+          edges?:
+            | Array<
+                | {
+                    __typename: "FriendsEdge";
+                    cursor: string;
+                    node?:
+                      | { __typename: "Droid"; id: string; name: string }
+                      | { __typename: "Human"; id: string; name: string }
+                      | null
+                      | undefined;
+                  }
+                | null
+                | undefined
+              >
+            | null
+            | undefined;
+          pageInfo: {
+            __typename: "PageInfo";
+            startCursor?: string | null | undefined;
+            endCursor?: string | null | undefined;
+          };
+        };
+      }
+    | null
+    | undefined;
+};
 
 export const GetAllHerosDocument = gql`
-    query getAllHeros {
-  hero {
-    id
-    __typename
-    name
-    friendsConnection {
+  query getAllHeros {
+    hero {
+      id
       __typename
-      totalCount
-      edges {
+      name
+      friendsConnection {
         __typename
-        cursor
-        node {
-          id
+        totalCount
+        edges {
           __typename
-          name
+          cursor
+          node {
+            id
+            __typename
+            name
+          }
         }
-      }
-      pageInfo {
-        __typename
-        startCursor
-        endCursor
+        pageInfo {
+          __typename
+          startCursor
+          endCursor
+        }
       }
     }
   }
-}
-    `;
+`;
 
 /**
  * __useGetAllHerosQuery__
@@ -317,14 +372,35 @@ export const GetAllHerosDocument = gql`
  *   },
  * });
  */
-export function useGetAllHerosQuery(baseOptions?: Apollo.QueryHookOptions<GetAllHerosQuery, GetAllHerosQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetAllHerosQuery, GetAllHerosQueryVariables>(GetAllHerosDocument, options);
-      }
-export function useGetAllHerosLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAllHerosQuery, GetAllHerosQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetAllHerosQuery, GetAllHerosQueryVariables>(GetAllHerosDocument, options);
-        }
+export function useGetAllHerosQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    GetAllHerosQuery,
+    GetAllHerosQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<GetAllHerosQuery, GetAllHerosQueryVariables>(
+    GetAllHerosDocument,
+    options
+  );
+}
+export function useGetAllHerosLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    GetAllHerosQuery,
+    GetAllHerosQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<GetAllHerosQuery, GetAllHerosQueryVariables>(
+    GetAllHerosDocument,
+    options
+  );
+}
 export type GetAllHerosQueryHookResult = ReturnType<typeof useGetAllHerosQuery>;
-export type GetAllHerosLazyQueryHookResult = ReturnType<typeof useGetAllHerosLazyQuery>;
-export type GetAllHerosQueryResult = Apollo.QueryResult<GetAllHerosQuery, GetAllHerosQueryVariables>;
+export type GetAllHerosLazyQueryHookResult = ReturnType<
+  typeof useGetAllHerosLazyQuery
+>;
+export type GetAllHerosQueryResult = Apollo.QueryResult<
+  GetAllHerosQuery,
+  GetAllHerosQueryVariables
+>;
