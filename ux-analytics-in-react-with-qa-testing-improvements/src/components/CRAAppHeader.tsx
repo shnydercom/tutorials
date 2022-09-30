2bbbb7eb944ddcff9a62165f63dedafe10844a78
shnyder <ajs.private@gmail.com>
2022-09-30 16:35:30 +0200

    nav shows user position

--- ux-analytics-in-react-with-qa-testing-improvements/src/components/CRAAppHeader.tsx
+++ ux-analytics-in-react-with-qa-testing-improvements/src/components/CRAAppHeader.tsx
@@ -1,34 +1,55 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "./../logo.svg";
 export const CRAAppHeader = () => {
+  const location = useLocation();
+  const pathName = location.pathname;
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        ...just another <br /> <code>create-react-app</code>
-        <br />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </p>
+      <div className="cra-rotator">
+        <p>
+          ...just another <br /> <code>create-react-app</code>
+          <br />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </div>
       <nav>
         <ul>
           <li>
-            <Link to="/">Intro/Readme</Link>
+            <Link className={pathName === "/" ? "current" : "other"} to="/">
+              Read me: Intro
+            </Link>
           </li>
           <li>
-            <Link to="/userpage">First: Users create usage-data</Link>
+            <Link
+              className={pathName === "/userpage" ? "current" : "other"}
+              to="/userpage"
+            >
+              Be a user: Create usage-data
+            </Link>
           </li>
           <li>
-            <Link to="/analytics">Then: Analysts check every detail</Link>
+            <Link
+              className={pathName === "/analytics" ? "current" : "other"}
+              to="/analytics"
+            >
+              Be an analyst: Check usage-event timings
+            </Link>
           </li>
           <li>
-            <Link to="/mixed">Finally: Make informed Product decisions</Link>
+            <Link
+              className={pathName === "/mixed" ? "current" : "other"}
+              to="/mixed"
+            >
+              Finally: Make informed Product decisions
+            </Link>
           </li>
         </ul>
       </nav>
