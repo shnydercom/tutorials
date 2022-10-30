bd1712733f64c813bf61e28f6c5d91911290af33
shnyder <ajs.private@gmail.com>
2022-10-30 12:09:37 +0100

    color switcher for diff betw UX and other events

--- ux-analytics-in-react-with-qa-testing-improvements/src/analytics/UXEventInterfaces.ts
+++ ux-analytics-in-react-with-qa-testing-improvements/src/analytics/UXEventInterfaces.ts
@@ -1,16 +1,20 @@
-export type UXEventType = "session_start" | "on_click" | "is_visible_changed_to" ;
+export type UXEventType =
+  | "session_start"
+  | "on_click"
+  | "is_visible_changed_to"
+  | "color_palette_changed_to";
 
 export interface UXEvent {
-	sourceID: string;
-	eventType: UXEventType;
-	eventValue: string;
+  sourceID: string;
+  eventType: UXEventType;
+  eventValue: string;
 }
 
 export interface PastUXEvent extends UXEvent {
-	timeStamp: number;
-	sessionID: string;
-	relativeTime: number;
-	firstEventTimeStamp: number;
+  timeStamp: number;
+  sessionID: string;
+  relativeTime: number;
+  firstEventTimeStamp: number;
 }
 
 export const UX_SOURCE_UNKNOWN = "source_unknown";
@@ -18,16 +22,19 @@ export const UX_SOURCE_UNKNOWN = "source_unknown";
 /**
  * UXEventType is a union type of string literals. This is useful for making switch-case statements exhaustive:
  * https://www.typescriptlang.org/docs/handbook/2/narrowing.html#exhaustiveness-checking
- * 
+ *
  * below however, we want to get an exhaustive list of all string literals in the union type. By building a helper
  * object with the key set to "s in UXEventType", we effectively create an enum-like structure with all the
- * string literals of UXEventType. When you create or change UXEventType, this helps to keep the 
+ * string literals of UXEventType. When you create or change UXEventType, this helps to keep the
  * array in UXEventTypeKeys up-to-date by throwing a typing error. The order of the entries is also preserved
  */
 const UXEventTypeHelperObj: { [s in UXEventType]: UXEventType } = {
-	"session_start": "session_start",
-	"on_click": "on_click",
-	"is_visible_changed_to": "is_visible_changed_to"
-}
+  session_start: "session_start",
+  on_click: "on_click",
+  is_visible_changed_to: "is_visible_changed_to",
+  color_palette_changed_to: "color_palette_changed_to"
+};
 
-export const UXEventTypeKeys: UXEventType[] = Object.keys(UXEventTypeHelperObj) as UXEventType[]
+export const UXEventTypeKeys: UXEventType[] = Object.keys(
+  UXEventTypeHelperObj
+) as UXEventType[];
--- ux-analytics-in-react-with-qa-testing-improvements/src/pages/UserPage.tsx
+++ ux-analytics-in-react-with-qa-testing-improvements/src/pages/UserPage.tsx
@@ -6,18 +6,46 @@ import { AGILE_MANIFESTO_FR as FR } from "../content-data/agileManifestoFR";
 import { AGILE_MANIFESTO_DE as DE } from "../content-data/agileManifestoDE";
 import { logDOMeventCallbackType } from "../analytics/UXEventLoggingAPI";
 import { DividerHR } from "./../components/semantic-html/DividerHR";
+import { useCallback, useRef, useState } from "react";
 
 interface UserPageProps {
-  logDOMeventCallback: logDOMeventCallbackType
+  logDOMeventCallback: logDOMeventCallbackType;
 }
 
+type ColorPalette = "dark" | "light";
+
 function UserPage(props: UserPageProps) {
-  const {logDOMeventCallback} = props;
+  const { logDOMeventCallback } = props;
+  const userPageRef = useRef<HTMLDivElement>(null);
+  const [colorMode, setColorMode] = useState<ColorPalette>("dark");
+
+  function switchColors() {
+    if (colorMode === "light") {
+      setColorMode("dark");
+      userPageRef.current?.style.setProperty("color", "white");
+      userPageRef.current?.style.setProperty("--base-bgcolor-a", "#00001f");
+      userPageRef.current?.style.setProperty("--base-bgcolor-b", "#004000");
+      userPageRef.current?.style.setProperty("--base-bgcolor-c", "#270000");
+      userPageRef.current?.style.setProperty("--base-bgcolor-d", "#292900");
+      userPageRef.current?.style.setProperty("--base-bgcolor-e", "#022f2f");
+      logDOMeventCallback("color_palette_changed_to", `${colorMode}`);
+      return;
+    }
+    setColorMode("light");
+    userPageRef.current?.style.setProperty("color", "black");
+    userPageRef.current?.style.setProperty("--base-bgcolor-a", "#ff6060");
+    userPageRef.current?.style.setProperty("--base-bgcolor-b", "#ffc36c");
+    userPageRef.current?.style.setProperty("--base-bgcolor-c", "#70ee70");
+    userPageRef.current?.style.setProperty("--base-bgcolor-d", "#9ecff6");
+    userPageRef.current?.style.setProperty("--base-bgcolor-e", "#92ecec");
+  }
+  //UX event handling
   function onHRVisible(isVisible: boolean, domElement?: Element) {
     logDOMeventCallback("is_visible_changed_to", `${isVisible}`, domElement);
   }
+
   return (
-    <div className="user-page">
+    <div className="user-page" ref={userPageRef}>
       <AgileManifestoDisplay content={EN} />
       <DividerHR onVisibilityChanged={onHRVisible} elementId="end-of-english" />
       <AgileManifestoDisplay content={ZH} isDense={true} className="ttb" />
@@ -29,14 +57,42 @@ function UserPage(props: UserPageProps) {
       <AgileManifestoDisplay content={DE} isDense={true} />
       <DividerHR onVisibilityChanged={onHRVisible} elementId="end-of-german" />
       <button
-        data-qa-id="the-end-button"
+        data-qa-id="button-lightswitch-at-bottom"
+        className="toggle-textbutton"
         onClick={(e) => {
-          console.log("well, so this button... it doesn't really do anything")
-          logDOMeventCallback("on_click", e.currentTarget.value, e.currentTarget)
-        }
-        }
+          switchColors();
+          //functionality can be independent of UX-events
+          logDOMeventCallback(
+            "on_click",
+            e.currentTarget.value,
+            e.currentTarget
+          );
+        }}
       >
-        <strong>This clickable element marks the end.</strong>
+        <strong>
+          {colorMode === "dark"
+            ? "Switch the lights on!"
+            : "Switch the lights off!"}
+        </strong>
+      </button>
+      <button
+        data-qa-id="button-floating-day-night"
+        className="toggle-iconbutton"
+        onClick={(e) => {
+          switchColors();
+          //functionality can be independent of UX-events
+          logDOMeventCallback(
+            "on_click",
+            e.currentTarget.value,
+            e.currentTarget
+          );
+        }}
+      >
+        <strong>
+          {colorMode === "dark"
+            ? "☾"
+            : "☼"}
+        </strong>
       </button>
     </div>
   );
