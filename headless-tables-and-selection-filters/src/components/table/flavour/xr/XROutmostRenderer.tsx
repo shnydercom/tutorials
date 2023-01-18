d71c2e1a64a6f32b41357fc5502ba5185e4c8d36
shnyder <ajs.private@gmail.com>
2023-01-18 02:09:25 +0100

    transparent background for AR added

--- headless-tables-and-selection-filters/src/components/table/flavour/xr/XROutmostRenderer.tsx
+++ headless-tables-and-selection-filters/src/components/table/flavour/xr/XROutmostRenderer.tsx
@@ -1,9 +1,9 @@
 import { Canvas } from "@react-three/fiber";
-import { useXR, Controllers  } from "@react-three/xr";
+import { useXR, Controllers } from "@react-three/xr";
 import { XR, ARButton } from "@react-three/xr/dist/XR";
 import React from "react";
 
-function useIsXRSubComponent() {
+export function useIsXRSubComponent() {
   try {
     useXR();
   } catch (error) {
@@ -16,15 +16,31 @@ export const XROutmostRenderer: React.FC<React.PropsWithChildren<{}>> = (
   props
 ) => {
   const isXRSubcomponent = useIsXRSubComponent();
+  const handleXRSessionstart = () => {
+    document.body.classList.add("remove-background");
+  };
+  const handleXRSessionEnd = () => {
+    document.body.classList.remove("remove-background");
+  };
   return (
     <>
       {isXRSubcomponent ? (
         props.children
       ) : (
         <>
-          <ARButton onError={(e) => console.error(e)} />
+          <ARButton
+            onError={(e) => console.error(e)}
+            sessionInit={{
+              requiredFeatures: ["local", "hit-test"],
+              optionalFeatures: ["dom-overlay", "dom-overlay-for-handheld-ar"],
+              domOverlay: { root: document.body },
+            }}
+          />
           <Canvas className="xr-canvas">
-            <XR>
+            <XR
+              onSessionStart={handleXRSessionstart}
+              onSessionEnd={handleXRSessionEnd}
+            >
               <ambientLight intensity={0.5} />
               <pointLight position={[5, 5, 5]} />
               <Controllers />
